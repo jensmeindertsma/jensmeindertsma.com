27f08ddf7c4d2e14d204e16e47e80a55178acfbd
Jens Meindertsma <mail@jensmeindertsma.com>
2023-06-01 14:10:25 +0200

    state

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,6 @@
 // @refresh reload
 import { Suspense } from "solid-js";
 import {
-  A,
   Body,
   ErrorBoundary,
   FileRoutes,
@@ -19,7 +18,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>Jens Meindertsma</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <Link rel="icon" type="image/png" href="/favicon.png" />
@@ -27,7 +26,6 @@ export default function Root() {
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <h1>ME!!!</h1>
             <Routes>
               <FileRoutes />
             </Routes>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,16 @@
-import { Title } from "solid-start";
-import Counter from "~/components/Counter";
-
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
+      <h1>Jens Meindertsma</h1>
+      <p>
+        Well, this isn&apos;t much of a portfolio site! I&apos;m still working
+        on turning this place into something meaningful.
+      </p>
+      <p>
+        <a href="https://github.com/jensmeindertsma/jensmeindertsma.com">
+          You can follow my progress on the GitHub page for this project.
+        </a>
+      </p>
     </main>
   );
 }
